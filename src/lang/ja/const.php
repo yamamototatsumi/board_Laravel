88a1fec663b29e376325badf3daf9533beb45575
Tarsumi Yamamoto <codetrain.tatsumi.yamamoto@gmail.com>
2022-08-01 21:34:58 +0900

    aaa

--- src/lang/ja/const.php
+++ src/lang/ja/const.php
@@ -4,7 +4,7 @@ return [
   'alrady'  => "既に登録されています",
   'exception' => '例外エラーが発生しました',
   'user'      => [
-      'name_size_error' => '名前はXX文字以内で入力してください。',
+      'name_size_error' => '名前はXX文字以内で入力してください。aaa',
   ]
 ];
 
